5a0abcb0ddde5ecc95f6590a1b21f77f6c9d265a
maie421 <maie421@naver.com>
2021-02-07 14:51:58 +0900

    localStorage 추가

--- findfriendfront/src/Components/Login/LoginContainer.js
+++ findfriendfront/src/Components/Login/LoginContainer.js
@@ -1,6 +1,32 @@
 import React, { useState } from "react";
 import LoginPresenter from "./LoginPresenter";
 import useInput from "../../Hooks/useInput";
+import { Login } from "../api";
+
+/*axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+
+const Login = (email, password) => {
+	const data = {
+		email,
+		password,
+	};
+	axios.post('/login', data)
+	.then(response => {
+		//console.log(response.data.data);
+		const { token } = response.data.data;
+        
+        console.log(token);
+		// API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+		axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+		// accessToken을 localStorage, cookie 등에 저장하지 않는다!
+		
+		//return response;
+	}).catch(error => {
+		// ... 에러 처리
+		return error;
+	});
+}*/
 
 const LoginContainer =()=> {
     const [state,onChange]= useInput({
@@ -12,7 +38,10 @@ const LoginContainer =()=> {
 
     const onSubmit = async (e)=>{
         e.preventDefault();
-        console.log(e);
+        const result=await Login(email,password);
+        if(result==='success'){
+            console.log(localStorage.getItem("token"));
+        }
     };
 
     return (
--- findfriendfront/src/Components/api.js
+++ findfriendfront/src/Components/api.js
@@ -5,20 +5,22 @@ axios.defaults.baseURL = "http://127.0.0.1:8000/api";
   // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   // axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 export const Login = (email, password) => {
+	const result='success';
 	const data = {
 		email,
 		password,
 	};
-	axios.post('/login', data).then(response => {
-		const { accessToken } = response.data;
+	axios.post('/login', data)
+	.then(response => {
+		//console.log(response.data.success);
+		const { token } = response.data.data;
 
 		// API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
-		axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-
-		// accessToken을 localStorage, cookie 등에 저장하지 않는다!
-
+		axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+		localStorage.setItem("token",token); // key-value 
 	}).catch(error => {
 		// ... 에러 처리
 		return error;
 	});
+	return result;
 }
